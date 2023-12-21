27c2170b863c4def873e515f71d7977734d6ab3e
Kim Gunil <lurker18@naver.com>
2023-12-22 01:44:05 +0900

    Update Custom_NER

---
+++ Code/SpaCy/Custom_NER.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics import accuracy_score
+import torch
+from torch.utils.data import Dataset, DataLoader
+from transformers import BertTokenizerFast, BertConfig, BertForTokenClassification
+from torch import cuda
+
+device = 'cuda' if cuda.is_available() else 'cpu'
+
+data_folder = 'F:/BioNER-Abbrev/Dataset/Scraped/nhsinform/'
+
+df = pd.read_csv(data_folder + 'nhsinform-IOB - Modified.txt', 
+                 sep = '\t', 
+                 header = None,
+                 names = ['Word', 'Annotation']
+                 )
+
+# Add a temporary 'sentence #' column for creating a joined words of sentences
+df['Sentence #'] = 'Sentence: 1'
+labels_to_ids = {k : v for v, k in enumerate(df.Annotation.unique())}
+ids_to_labels = {v : k for v, k in enumerate(df.Annotation.unique())}
+
+df['Word'] = df['Word'].astype('string')
+df['Annotation'] = df['Annotation'].astype('string')
+
+#index = df['Annotation'].index[df['Annotation'].apply(pd.isnull)]
+
+# Create a new column called sentence which groups the words by sentence
+df['sentence'] = df[['Sentence #', 'Word', 'Annotation']].groupby(['Sentence #'])['Word'].transform(lambda x : ' '.join(x))
+# Also, create a new column called 'word_labels' which groups the tags by sentence
+df['word_labels'] = df[['Sentence #', 'Word', 'Annotation']].groupby(['Sentence #'])['Annotation'].transform(lambda x : ','.join(x))
+df
+
+
+df = df[['sentence', 'word_labels']].drop_duplicates().reset_index(drop = True)
+
+df.to_json(data_folder + 'NHSInform-JSON.json', 
+           orient = 'records', 
+           lines = True)
+df.head()
+
+len(df)
