77076caa4ab005055b44a0efae770676bdacbb33
Kim Gunil <lurker18@naver.com>
2023-12-23 12:57:32 +0900

    Add Custom_NER_RoBERTa and updated nhsinform dataset

--- Code/SpaCy/Custom_NER.py
+++ Code/SpaCy/Custom_NER.py
@@ -15,11 +15,18 @@ df = pd.read_csv(data_folder + 'nhsinform-IOB - Modified - Copy.txt',
                  header = None,
                  names = ['Word', 'Annotation']
                  )
-df.fillna
 
+df['number'] = 0
+sentence_number = 1
+for index, row in df.iterrows():
+    if row['Word'] == '###':
+        df.at[index, 'number'] = sentence_number
+        sentence_number += 1
+    else:
+        df.at[index, 'number'] = sentence_number
 
 # Add a temporary 'sentence #' column for creating a joined words of sentences
-df['Sentence #'] = 'Sentence: 1'
+df['Sentence #'] = 'TEMP'
 labels_to_ids = {k : v for v, k in enumerate(df.Annotation.unique())}
 ids_to_labels = {v : k for v, k in enumerate(df.Annotation.unique())}
 
@@ -29,11 +36,15 @@ df['Annotation'] = df['Annotation'].astype('string')
 #index = df['Annotation'].index[df['Annotation'].apply(pd.isnull)]
 
 # Create a new column called sentence which groups the words by sentence
-df['sentence'] = df[['Sentence #', 'Word', 'Annotation']].groupby(['Sentence #'])['Word'].transform(lambda x : ' '.join(x))
+df['sentence'] = df[['Sentence #', 'Word', 'Annotation', 'number']].groupby(['number'])['Word'].transform(lambda x : ' '.join(x))
 # Also, create a new column called 'word_labels' which groups the tags by sentence
-df['word_labels'] = df[['Sentence #', 'Word', 'Annotation']].groupby(['Sentence #'])['Annotation'].transform(lambda x : ','.join(x))
+df['word_labels'] = df[['Sentence #', 'Word', 'Annotation', 'number']].groupby(['number'])['Annotation'].transform(lambda x : ','.join(x))
 
 df = df[['sentence', 'word_labels']].drop_duplicates().reset_index(drop = True)
+
+df['sentence'] = df['sentence'].apply(lambda x: x.replace(' ###', ''))
+df['word_labels'] = df['word_labels'].apply(lambda x: x.replace(',###', ''))
+
 
 df.to_json(data_folder + 'NHSInform-JSON.json', 
            orient = 'records', 
