f640ff0ce311453d52ba705e60bfd50fa2ee7a53
lurker18 <lurker18@naver.com>
2024-01-04 02:08:12 +0900

    Add SpaCy-LLM models - Llama2-13B and Falcon-7B

--- Code/SpaCy-LLM/Llama2-13B/Llama2-13B.py
+++ Code/SpaCy-LLM/Llama2-13B/Llama2-13B.py
@@ -1,7 +1,7 @@
 from spacy_llm.util import assemble
 
 # Assemble a spaCy pipeline from the config
-nlp = assemble("Code/SpaCy-LLM/config.cfg")
+nlp = assemble("Code/SpaCy-LLM/Llama2-13B/config.cfg")
 
 # Use this pipeline as you would normally
 doc = nlp("Addison's disease (also known as primary adrenal insufficiency or hypoadrenalism) is a rare disorder of the adrenal glands. The adrenal glands are two small glands that sit on top of the kidneys. They produce essential hormones: cortisol, aldosterone and adrenaline. In Addison's disease, the adrenal gland is damaged, and not enough cortisol and aldosterone are produced. About 8,400 people in the UK have Addison's disease. It can affect people of any age, although it's most common between the ages of 30 and 50.")
