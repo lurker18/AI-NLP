2e5a0491b15022ef4b24f1c748b8990f0a714b6b
lurker18 <lurker18@naver.com>
2024-03-19 22:41:16 +0900

    New Update RAG

--- Code/UMLS/UMLS_Extractor.py
+++
@@ -1,6 +0,0 @@
-import umls_api
-import pandas as pd
-
-resp = umls_api.API(api_key = '2cac3304-aeb8-426e-a508-47ecb4c7c310').get_cui(cui='C4304383')
-pd.DataFrame(resp)
-pd.DataFrame(pd.DataFrame(resp).result['inverseInheritedRelations'])
--- Code/UniversalNER/UniversalNER.py
+++
@@ -1,5 +0,0 @@
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-tokenizer = AutoTokenizer.from_pretrained("Universal-NER/UniNER-7B-all")
-model = AutoModelForCausalLM.from_pretrained("Universal-NER/UniNER-7B-all")
